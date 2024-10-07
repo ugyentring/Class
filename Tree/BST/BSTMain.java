605e896f8c8a55a3284cfd45fa7056418d77e62d
ugyen.dev <dev.ugyen@gmail.com>
2024-10-07 10:53:03 +0600

    implemented insert method

--- Tree/BST/BSTMain.java
+++ Tree/BST/BSTMain.java
@@ -11,17 +11,56 @@ class Node {
     }
 }
 
-//binary searcg terr class
+//binary search tree class
 class BST {
     Node root;
 
-  
+    //insert method
+    public boolean insert(int data) {
+        Node newNode = new Node(data);
+
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+
+        Node temp = root;
+        while (true) {
+            if (newNode.data == temp.data) {
+                return false;
+            }
+
+            if (newNode.data < temp.data) {
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+            } else {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+
+    }
 }
 
 class BSTMain {
     public static void main(String[] args) {
         BST bst = new BST();
 
-        System.out.println(bst.root);
+        bst.insert(47);
+        bst.insert(21);
+        bst.insert(76);
+        bst.insert(18);
+        bst.insert(27);
+        bst.insert(52);
+        bst.insert(82);
+        bst.insert(27);
+
+        System.out.println(bst.root.data);
     }
 }
